d22225a45256cdb35acbca1047d48a49ce63bf87
Jack Yu <jackyu@google.com>
2020-05-12 00:08:26 -0700

    Added geographical scope duplicate support
    Added geographical scope for duplicate support per
    3GPP TS 23.041 section 9.4.1.2.1.
    
    Fix: 155752045
    Test: CellBroadcastServiceTests
    Change-Id: Ic11ff67f2a4abacecf34188d6f916fb18e4cf3f0

--- tests/src/com/android/cellbroadcastservice/tests/CellBroadcastHandlerTest.java
+++ tests/src/com/android/cellbroadcastservice/tests/CellBroadcastHandlerTest.java
@@ -135,7 +135,7 @@ public class CellBroadcastHandlerTest extends CellBroadcastServiceTestBase {
     private SmsCbMessage createSmsCbMessage(int serialNumber, int serviceCategory,
             String messageBody) {
         return new SmsCbMessage(SmsCbMessage.MESSAGE_FORMAT_3GPP,
-                0, serialNumber, new SmsCbLocation(),
+                0, serialNumber, new SmsCbLocation("311480", 0, 0),
                 serviceCategory, "en", messageBody, 3,
                 null, null, 0, 1);
     }
@@ -166,6 +166,14 @@ public class CellBroadcastHandlerTest extends CellBroadcastServiceTestBase {
     public void testNotDuplicateMessageBodyDifferent() throws Exception {
         putResources(com.android.cellbroadcastservice.R.bool.duplicate_compare_body, true);
         SmsCbMessage msg = createSmsCbMessage(1234, 4370, "msg");
+        assertFalse(mCellBroadcastHandler.isDuplicate(msg));
+    }
+
+    public void testNotDuplicateCellLocationDifferent() throws Exception {
+        SmsCbMessage msg = new SmsCbMessage(SmsCbMessage.MESSAGE_FORMAT_3GPP,
+                0, 1234, new SmsCbLocation("311480", 0, 1),
+                4370, "en", "Test Message", 3,
+                null, null, 0, 1);
         assertFalse(mCellBroadcastHandler.isDuplicate(msg));
     }
 }
